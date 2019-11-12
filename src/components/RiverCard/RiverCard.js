bc291543ce14eba2ba0430180929fb54d82d3fd3
Jesse Packwood <packwoodjesse@gmail.com>
2019-11-12 00:57:11 -0700

    create formate for RiverCard component

---
+++ src/components/RiverCard/RiverCard.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './RiverCard.scss'
+
+const RiverCard = (props) => {
+
+    const mapRivers = props.rivers.map( (river, index) => {
+            return (
+                <div className="river-card" key={index}>
+                    <h2>{river.name}</h2>
+                    <h2>Flow{river.flows}</h2>
+                    <h2>Weather {river.weather}{'\u00b0'}F</h2>
+                    <p>HIGH{river.high} {'\u00b0'}F</p>
+                    <p>Low: {river.low} {'\u00b0'}F</p>
+                    <p>Wind: {river.wind}mph</p>
+                </div>
+            )
+        })
+
+    return (
+        <div>
+            {mapRivers}
+        </div>
+    )
+}
+
+export default RiverCard;
